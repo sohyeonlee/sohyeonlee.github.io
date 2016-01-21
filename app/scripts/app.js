2149d9ecb520ac48d47cb7fac4a5862f4eadb33a
Irene Lee <sohyeonlee@berkeley.edu>
2016-01-21 01:31:17 -0800

    updated projects

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,36 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/ridemate', {
+        templateUrl: 'views/ridemate.html',
+        controller: 'RidemateCtrl',
+        controllerAs: 'ridemate'
+      })
+      .when('/miisik', {
+        templateUrl: 'views/miisik.html',
+        controller: 'MiisikCtrl',
+        controllerAs: 'miisik'
+      })
+      .when('/tastehome', {
+        templateUrl: 'views/tastehome.html',
+        controller: 'TastehomeCtrl',
+        controllerAs: 'tastehome'
+      })
+      .when('/expedia', {
+        templateUrl: 'views/expedia.html',
+        controller: 'ExpediaCtrl',
+        controllerAs: 'expedia'
+      })
+      .when('/pickeat', {
+        templateUrl: 'views/pickeat.html',
+        controller: 'PickeatCtrl',
+        controllerAs: 'pickeat'
+      })
+      .when('/cs61as-quiz-system', {
+        templateUrl: 'views/cs.html',
+        controller: 'CsCtrl',
+        controllerAs: 'cs'
+      })
       .otherwise({
         redirectTo: '/'
       });
